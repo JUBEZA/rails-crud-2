a3e08fd69617d4e3a4a5e6826a12163713ddded3
Juan Bezanilla <juanbezanillaz@gmail.com>
2024-02-01 14:26:58 -0300

    restaurant created

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,33 @@
+class RestaurantsController < ApplicationController
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.save
+    redirect_to restaurant_path(@restaurant)
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :location, :rating)
+  end
+  # private
+
+  # def restaurans_params
+  #   params.require(:restaurant).permit(:name,:adress,:rating)
+  # end
+
+
+end
